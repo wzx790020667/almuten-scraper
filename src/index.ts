170da9a5afdda560085ef503a82e90ec416da85b
oliver <zexinwan7@outlook.com>
2025-03-16 15:37:46 +0800

    prepare for npm publish

--- src/index.ts
+++ src/index.ts
@@ -5,48 +5,21 @@
  * @module almuten-scraper
  */
 
-import { AlmutenScraper } from "./almutenScraper";
-import {
-  type BirthInfo,
-  type HoroscopeData,
-  type ScraperOptions,
-} from "./types";
-
 /**
  * 获取星盘数据的快捷函数
  * @param birthInfo 出生信息
  * @param options 爬虫选项
  * @returns Promise<HoroscopeData> 星盘数据
  */
-// export async function getHoroscope(
-//   birthInfo: BirthInfo,
-//   options?: ScraperOptions
-// ): Promise<HoroscopeData> {
-//   const scraper = new AlmutenScraper({
-//     birthInfo,
-//     options,
-//   });
-
-//   try {
-//     const horoscopeData = await scraper.getHoroscope(birthInfo);
-//     return horoscopeData;
-//   } finally {
-//     await scraper.close();
-//   }
-// }
 
 // 导出所有类型和工具函数
 export { AlmutenScraper } from "./almutenScraper";
+export { formatDate, validateDate, formatCoordinates } from "./utils/dateFormatter";
 export {
-  formatDate,
-  validateDate,
-  formatCoordinates,
-} from "./utils/dateFormatter";
-export {
-  type BirthInfo,
-  type HoroscopeData,
-  type ScraperOptions,
-  type PlanetInfo,
-  type HouseInfo,
-  type AspectInfo,
+	type BirthInfo,
+	type HoroscopeData,
+	type ScraperOptions,
+	type PlanetInfo,
+	type HouseInfo,
+	type AspectInfo,
 } from "./types";
